c981f6da94830d0a23f77da76eaac04b8c247293
Carlota Pearl <carlota.pearl@gmail.com>
2017-07-26 21:54:00 -0400

    fixed errors with unclosed tags

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mustacheExpress = require('mustache-express');
-const data = require('./data.js');
+const data = require('./data.js');// he has "./data"
 const app = express();
 
 app.engine('mustache', mustacheExpress());
@@ -8,16 +8,17 @@ app.set('views', './views');
 app.set('view engine', 'mustache');
 
 app.use('/public', express.static('public'));
-
+//listening on root
 app.get('/', function(req, res){
-  res.render('index', data)
+  //index.mustache render
+  res.render('index', data)//data.users
 });
-
+//look in data and find username matches the request's username
 app.get('/:id', function(req, res) {
   var user = {};
   for (let i = 0; i < data.users.length; i++) {
     user = data.users[i];
-
+    //Ben used const user =data.users.find(function(user){return user.username ==== req.params.id})
     if (user.id == req.params.id) {
       break;
     }
